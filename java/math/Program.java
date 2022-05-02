6f1705005606297f0e4dbddcbbde44e0e8746ef9
andrewrdev <84985099+andrewrdev@users.noreply.github.com>
2022-05-02 17:53:39 -0300

    update commit

---
+++ java/math/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+        double numberDouble = 52.45;
+
+        System.out.println(Math.ceil(numberDouble));
+        System.out.println(Math.floor(numberDouble));
+        System.out.println(Math.round(numberDouble));
+        System.out.println(Math.random() * 200);
+    }
+}
